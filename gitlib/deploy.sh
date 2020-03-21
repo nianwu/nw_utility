2d9562a1550ea7f532ee904f0826073918485350
nianwu <31959790+nianwu@users.noreply.github.com>
2020-03-21 16:34:29 +0800

    更新gitlab 部署脚本, 在部署的时候, 创建对应的挂载目录

--- gitlib/deploy.sh
+++ gitlib/deploy.sh
@@ -1,1 +1,4 @@
-docker stack deploy --compose-file docker-compose.yml gitlab
+mkdir data/data \
+&& mkdir data/config \
+&& mkdir data/logs \
+&& docker stack deploy --compose-file docker-compose.yml gitlab
